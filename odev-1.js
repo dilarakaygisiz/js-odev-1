d9d84cde367208b673d7e24151e99239e40b55c4
dilarakaygisiz <dilaraakaygisiz@gmail.com>
2024-12-24 15:15:44 +0300

    first commit

---
+++ odev-1.js
@@ -0,0 +1,58 @@
+// **** ODEV - 1 *****
+let firstName = prompt("")
+let greeting = document.querySelector("#welcome")
+greeting.innerHTML = `Merhaba, ${firstName}! Hoşgeldin!`
+
+
+
+setInterval(showTime, 1000);
+        function showTime() {
+            let time = new Date();
+            let hour =
+                time.getHours();
+            let min =
+                time.getMinutes();
+            let sec =
+                time.getSeconds();
+            am_pm = " Salı";
+        
+            if (hour >= 12) {
+                if (hour > 12)
+                    hour -= 12;
+                Salı_pm = " Salı";
+            } else if (hour == 0) {
+                hr = 12;
+                am_pm = " AM";
+            }
+        
+            hour =
+                hour < 10
+                    ? "0" + hour
+                    : hour;
+            min =
+                min < 10
+                    ? "0" + min
+                    : min;
+            sec =
+                sec < 10
+                    ? "0" + sec
+                    : sec;
+        
+            let currentTime =
+                hour +
+                ":" +
+                min +
+                ":" +
+                sec +
+                am_pm;
+        
+            document.getElementById(
+                "clock"
+            ).innerHTML =
+                currentTime;
+
+
+        }
+        
+        showTime();
+
